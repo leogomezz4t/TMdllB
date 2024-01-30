558c4cd7f52d2d44db75439ae0e6da058e997830
Leonardo Gomez <leojwu18@gmail.com>
2024-01-30 00:32:08 -0700

    inherits from ResultContainer

---
+++ TMdllB/MovieResultContainer.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB
+{
+    public class MovieResultContainer : ResultContainer
+    {
+        /*
+         * A list of type MovieResult that represents the results returned
+         */
+        [JsonPropertyName()]
+    }
+}
