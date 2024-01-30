4780508d5359ef3c5f56a361eef5d52204f1c5e9
Leonardo Gomez <leojwu18@gmail.com>
2024-01-30 00:05:41 -0700

    added SearchMovies method

---
+++ TMdllB/TMDbClient.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+
+namespace TMdllB
+{
+    // **** Constants
+    
+
+    public class TMDbClient
+    {
+        public TMDbClient(string apiKey)
+        {
+            API_KEY = apiKey;
+        }
+
+        // **** Class Constants
+        private static string API_BASE_URL = "https://api.themoviedb.org/3";
+        private static string API_SEARCH = "/search";
+        private static string API_MOVIES = "/movie";
+        private static string API_TV_SERIES = "/tv";
+
+        // **** Properties
+
+        private string API_KEY;
+
+        // **** Methods
+        public async Task<MovieContainer> SearchMovies(string query)
+        {
+            // Url Encode the get params
+            string encodedQuery = HttpUtility.UrlEncode(query);
+            // Construct the url
+            string url = $"{API_BASE_URL}{API_SEARCH}{API_MOVIES}?api_key={API_KEY}&query={encodedQuery}";
+            Console.WriteLine($"URL:  {url}");
+            // Get string result from the url
+            string jsonResult = await QueryAPI.GetString(url);
+            // ------ TODO Make sure the result is succesful
+            // Serialize the jsonResult
+            MovieContainer container = QueryParser.ParseMovieResults(jsonResult);
+
+            return container;
+        }
+    }
+}
