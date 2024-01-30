f01a6e5014954a2dcb0f9c166df056bcc0b9cf90
Leonardo Gomez <leojwu18@gmail.com>
2024-01-30 00:09:01 -0700

    bare result container

---
+++ TMdllB/MovieResult.cs
@@ -0,0 +1,21 @@
+﻿namespace TMdllB
+{
+    public class MovieResult
+    {
+        public bool Adult { get; set; }
+        public string BackdropPath { get; set; } = string.Empty;
+        public int[] GenreIds { get; set; }
+        public int Id { get; set; }
+        public string OriginalLanguage { get; set; } = string.Empty;
+        public string OriginalTitle { get; set; } = string.Empty;
+        public string Overview {  get; set; } = string.Empty;
+        public float Popularity { get; set; }
+        public string PosterPath { get; set; } = string.Empty;
+        public DateOnly ReleaseData { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool Video { get; set; }
+        public float VoteAverage { get; set; }
+        public int VoteCount { get; set; }
+
+    }
+}
