b6eddf209d9acc917ad6e1f646a5782efbd0c074
Leonardo Gomez <leojwu18@gmail.com>
2024-01-30 00:07:02 -0700

    added ParseMovieResults method

---
+++ TMdllB/QueryParser.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace TMdllB
+{
+    public static class QueryParser
+    {
+        // **** Static Properties
+        private static readonly JsonSerializerOptions _serializeOptions = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public static MovieContainer ParseMovieResults(string json)
+        {
+            MovieContainer mc = JsonSerializer.Deserialize<MovieContainer>(json, _serializeOptions);
+
+            return mc;
+        }
+    }
+}
