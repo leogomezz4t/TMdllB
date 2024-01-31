a95566601ecb1967e0e73fb05759e4d5e1212cf5
Leonardo Gomez <leojwu18@gmail.com>
2024-01-31 12:55:15 -0700

    created a movie details class that derives from TvMovieBaseDetails

---
+++ TMdllB/Details/MovieDetails.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+using TMdllB.Objects;
+
+namespace TMdllB.Details
+{
+    public class MovieDetails : TvMovieBaseDetails
+    {
+        [JsonPropertyName("belongs_to_collection")]
+        public Collection? BelongsToCollection { get; }
+
+        [JsonPropertyName("budget")]
+        public int Budget { get; }
+
+        [JsonPropertyName("imdb_id")]
+        public string ImdbId { get; }
+
+        [JsonPropertyName("original_title")]
+        public string OriginalTitle { get; }
+
+        [JsonPropertyName("release_date")] 
+        public DateOnly ReleaseDate { get; }
+
+        [JsonPropertyName("revenue")]
+        public int Revenue { get; }
+
+        [JsonPropertyName("runtime")]
+        public int Runtime { get; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; }
+
+        [JsonPropertyName("video")]
+        public bool HasVideo { get; }
+    }
+}
