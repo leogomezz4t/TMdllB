03b3c39b8f40c6690710a6699a2bff366e66ac33
Leonardo Gomez <leojwu18@gmail.com>
2024-01-31 12:58:57 -0700

    created a SpokenLanguage the serializes the json in a details object

---
+++ TMdllB/Objects/SpokenLanguage.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB.Objects
+{
+    public class SpokenLanguage
+    {
+        [JsonPropertyName("english_name")]
+        public string EnglishName { get; }
+
+        [JsonPropertyName("iso_639_1")]
+        public string Iso639_1 { get; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; }
+    }
+}
