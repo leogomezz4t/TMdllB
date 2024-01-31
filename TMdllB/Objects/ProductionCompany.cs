0134b310c9a7c50f81f98ba3425833fb97c75e22
Leonardo Gomez <leojwu18@gmail.com>
2024-01-31 12:58:12 -0700

    created ProductionCompany class that serializes the json in the a details object

---
+++ TMdllB/Objects/ProductionCompany.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB.Objects
+{
+    public class ProductionCompany
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; }
+
+        [JsonPropertyName("logo_path")]
+        public string? RelativeLogoPath { get; }
+
+        public string? LogoPath { get
+            {
+                if (RelativeLogoPath == null) return null;
+
+                return $"{TMDbClient.IMAGE_BASE_URL}{TMDbClient.IMAGE_ORIGINAL_SIZE_ENDPOINT}{RelativeLogoPath}";
+            }
+        }
+
+        [JsonPropertyName("name")]
+        public string Name { get; }
+
+        [JsonPropertyName("origin_country")]
+        public string OriginCountry { get; }
+    }
+}
