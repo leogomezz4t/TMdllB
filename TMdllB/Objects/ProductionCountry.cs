e99fc0dfd9a60d495fe307b63ab3c4314fc17eb8
Leonardo Gomez <leojwu18@gmail.com>
2024-01-31 12:58:37 -0700

    created a ProductionCountry class that serializes the json in a details object

---
+++ TMdllB/Objects/ProductionCountry.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB.Objects
+{
+    public class ProductionCountry
+    {
+        [JsonPropertyName("iso_3166_1")]
+        public string Iso3166_1 { get; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; }
+    }
+}
