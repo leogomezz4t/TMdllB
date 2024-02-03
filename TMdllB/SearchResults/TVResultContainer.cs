fb959a5e6c3a0cbd3c74aa23e630018d90e1a0fe
Leonardo Gomez <leojwu18@gmail.com>
2024-02-03 14:57:34 -0700

    created new TVResultContainer that inherits from ResultContainer

--- TMdllB/SearchResults/TVResultContainer.cs
+++ TMdllB/SearchResults/TVResultContainer.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace TMdllB.SearchResults
 {
+    /*
+     * TVResultContainer represents the object returned by the TMDB API when using the /search/tv endpoint
+     */
     public class TVResultContainer : ResultContainer
     {
-
+        /*
+         *  A list of type TVResult that represents the results returned
+         */
+        [JsonPropertyName("result")]
+        public List<TVResult> Results { get; }
     }
 }
