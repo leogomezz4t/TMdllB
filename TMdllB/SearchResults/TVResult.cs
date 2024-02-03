fc1bc02638595b0f5cad32b73aeef07e20fd64ac
Leonardo Gomez <leojwu18@gmail.com>
2024-02-03 14:53:03 -0700

    created new TVResult that inherits from TVMovieBaseResult.cs

---
+++ TMdllB/SearchResults/TVResult.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB.SearchResults
+{
+    public class TVResult
+    {
+        /*
+         * List of the countries the tv show originated from
+         */
+        [JsonPropertyName("origin_country")]
+        public string[] OriginCountries { get; }
+
+        /*
+         * The original name of the show
+         */
+        [JsonPropertyName("original_name")]
+        public string OriginalName { get; }
+
+        /*
+         * The date the first episode aired
+         */
+        [JsonPropertyName("first_air_date")]
+        public DateOnly FirstAirDate { get; }
+
+        /*
+         * The name fo the show
+         */
+        [JsonPropertyName("name")]
+        public string Name { get; }
+    }
+}
