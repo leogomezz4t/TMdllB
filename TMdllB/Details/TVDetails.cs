d8120d5fb8e1f0b507b5afb0f6f8fd6153ebc372
Leonardo Gomez <leojwu18@gmail.com>
2024-02-19 00:27:21 -0700

    WIP not all properties added

--- TMdllB/Details/TVDetails.cs
+++ TMdllB/Details/TVDetails.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Text.Json.Serialization;
+using TMdllB.Objects;
 
 namespace TMdllB.Details
 {
-    internal class TvDetails
+    public class TvDetails
     {
+        [JsonPropertyName("created_by")]
+        public Creator[] CreatedBy { get; set; }
+
+        //[JsonPropertyName("episode_run_time")]
+        // Not sure what typa value goes here
+
+        [JsonPropertyName("first_air_date")]
+        public DateOnly FirstAirDate { get; set; }
+
+        [JsonPropertyName("in_production")
+        public bool InProduction { get; set; }
+
+        [JsonPropertyName("languages")]
+        public string[] Languages { get; set; }
+
+        [JsonPropertyName("last_air_date")]
+        public DateOnly LastAirDate { get; set; }
+
+        [JsonPropertyName("last_episode_to_air")]
+        public Episode LastEpisodeToAir { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("next_episode_to_air")]
+        public Episode? NextEpisodeToAir { get; set; }
+
+
     }
 }
