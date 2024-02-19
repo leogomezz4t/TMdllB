70171c9d1f8f1b3556bb263e49de0b78c3fdd0c9
Leonardo Gomez <leojwu18@gmail.com>
2024-02-19 00:26:26 -0700

    created object for serializing the json of a tv episode

---
+++ TMdllB/Objects/Episode.cs
@@ -0,0 +1,53 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace TMdllB.Objects
+{
+    public class Episode
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("overview")]
+        public string Overview { get; set; }
+
+        [JsonPropertyName("vote_average")]
+        public float VoteAverage { get; set; }
+
+        [JsonPropertyName("vote_count")]
+        public int VoteCount { get; set; }
+
+        [JsonPropertyName("air_date")]
+        public DateOnly AirDate { get; set; }
+
+        [JsonPropertyName("episode_number")]
+        public int EpisodeNumber { get; set; }
+
+        [JsonPropertyName("episode_type")]
+        public string EpisodeType { get; set; }
+
+        [JsonPropertyName("production_code")]
+        public string ProductionCode { get; set; }
+
+        [JsonPropertyName("runtime")]
+        public int Runtime { get; set; }
+
+        [JsonPropertyName("season_number")]
+        public int SeasonNumber { get; set; }
+
+        [JsonPropertyName("show_id")]
+        public int ShowId { get; set; }
+
+        [JsonPropertyName("still_path")]
+        public string RelativeStillPath { get; set; }
+
+        public string StillPath { get
+            {
+                return TMDbClient.GetOriginalImageUrl(RelativeStillPath);
+            } }
+    }
+}
