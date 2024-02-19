f11d583ec4121f58421245ccb7a3fd122d07d57e
Leonardo Gomez <leojwu18@gmail.com>
2024-02-19 14:15:04 -0700

    created Network and Season objects

---
+++ TMdllB/Objects/Network.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB.Objects
+{
+    public class Network
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("logo_path")]
+        public string RelativeLogoPath { get; set; }
+
+        public string LogoPath { get
+            {
+                return TMDbClient.GetOriginalImageUrl(RelativeLogoPath);
+            }
+        }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("origin_country")]
+        public string OriginCountry { get; set; }
+    }
+}
---
+++ TMdllB/Objects/Season.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB.Objects
+{
+    public class Season
+    {
+        [JsonPropertyName("air_date")]
+        public DateOnly? AirDate { get; set; }
+
+        [JsonPropertyName("episode_count")]
+        public int EpisodeCount { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("overview")]
+        public string Overview { get; set; }
+
+        [JsonPropertyName("poster_path")]
+        public string RelativePosterPath { get; set; }
+
+        public string PosterPath { get
+            {
+                return TMDbClient.GetOriginalImageUrl(RelativePosterPath);
+            }
+        }
+
+        [JsonPropertyName("season_number")]
+        public int SeasonNumber { get; set; }
+
+        [JsonPropertyName("vote_average")]
+        public float VoteAverage { get; set; }
+    }
+}
