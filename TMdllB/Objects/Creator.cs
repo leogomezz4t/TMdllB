72252c1c08f905d5c75a1e9a354c0a0810d756a6
Leonardo Gomez <leojwu18@gmail.com>
2024-02-19 00:26:04 -0700

    created object for serializing the json of a tv show creator

---
+++ TMdllB/Objects/Creator.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB.Objects
+{
+    public class CreatedBy
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("credit_id")]
+        public string CreditId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("gender")]
+        public int Gender { get; set; }
+
+        [JsonPropertyName("profile_path")]
+        public string RelativeProfilePath { get; set; }
+
+        public string ProfilePath { get
+            {
+                return $"{TMDbClient.IMAGE_BASE_URL}{TMDbClient.IMAGE_ORIGINAL_SIZE_ENDPOINT}{RelativeProfilePath}";
+            } }
+    }
+}
