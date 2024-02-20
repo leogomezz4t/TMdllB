edc76884f3661ef8f414e9ae848668c42591aeaf
Leonardo Gomez <leojwu18@gmail.com>
2024-02-20 16:23:28 -0700

    created custom Exception for QueryParser

---
+++ TMdllB/Exceptions/DeserializationFailureException.cs
@@ -0,0 +1,10 @@
+﻿namespace TMdllB.Exceptions
+{
+    public class DeserializationFailureException : Exception
+    {
+        public DeserializationFailureException() : base() { }
+
+        public DeserializationFailureException(string json) : base($"Failed to deserialize the input: {json}") { }
+        
+    }
+}
