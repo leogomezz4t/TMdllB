7b01d1bac5e0a265d2fb158093e1f2653813d15e
Leonardo Gomez <leojwu18@gmail.com>
2024-02-23 20:59:16 -0700

    alternative titles objects

---
+++ TMdllB/Objects/AlternativeTitle.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB.Objects
+{
+    public class AlternativeTitle
+    {
+        [JsonPropertyName("iso_3166_1")]
+        public string Iso3166_1 { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
---
+++ TMdllB/Objects/AlternativeTitles.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TMdllB.Objects
+{
+    public class AlternativeTitles
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("titles")]
+        public AlternativeTitle[] Titles { get; set; }
+    }
+}
